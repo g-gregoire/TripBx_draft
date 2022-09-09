96ad7009799e77c4a72b45f011f666809ac394f0
Georges Gregoire <georges.e.gregoire@gmail.com>
2022-09-09 16:18:29 -0400

    Initial Commit

---
+++ TripBx_draft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripBx_draft
+//
+//  Created by Georges Gregoire on 2022-09-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripBx_draft/TripBx_draftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripBx_draftApp.swift
+//  TripBx_draft
+//
+//  Created by Georges Gregoire on 2022-09-09.
+//
+
+import SwiftUI
+
+@main
+struct TripBx_draftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
